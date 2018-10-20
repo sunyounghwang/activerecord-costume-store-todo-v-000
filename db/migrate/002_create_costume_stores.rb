a3c3fd41c45af6e8b2e29d2270f979929e64cae3
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-20 00:25:16 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,8 +1,8 @@
 class CreateCostumeStores < ActiveRecord::Base
   def change
     create_table :costume_stores do |t|
-      t.string :name 
-      t.string :location 
+      t.string :name
+      t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
