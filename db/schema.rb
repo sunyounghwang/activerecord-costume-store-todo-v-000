9d1eb09dda1940e62dc900130cff332541f2ba41
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-20 00:32:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -31,4 +31,15 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.integer "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.string "description"
+  end
+
 end
